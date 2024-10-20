b7278b25b7740021745101145b3f8f8a5116ea69
ajbox <56485698+ajbox@users.noreply.github.com>
2024-10-20 16:50:12 -0400

    adding java and jquery from laptop

---
+++ jquery/main.js
@@ -0,0 +1,22 @@
+$(".dark-btn").click(function(){
+    $("body").toggleClass("dark");
+    $(".box").toggleClass("dark");
+  }
+);
+
+$(".spin-btn").click(
+    function(){
+        $(".box").toggleClass("spin");
+    }
+)
+
+$(".reveal-btn").click(
+    function(){
+        $(".bubblebuddy").addClass("reveal");
+        //$(".reveal-btn").css("display","none");
+        $(".reveal-btn").hide();
+    }
+);
+
+
+ $( ".draggable" ).draggable();
